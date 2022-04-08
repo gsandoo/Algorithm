d6084882361c0d4775ed74ae8e4c6cb53d431c51
gsandoo <rogersddt@naver.com>
2022-04-08 22:06:07 +0900

    python 3

--- 동적 프로그래밍/2156 포도주.py
+++ 동적 프로그래밍/2156 포도주.py
@@ -25,6 +25,16 @@
 # 33
 
 
-# 와인이 담긴 양 arr 배열과 총 량 dp 배열 두개를 만들어준다.
+#  핵심 ** 와인이 담긴 양 arr 배열과 와인을 합한 값 dp 배열 두개를 만들어준다.
 
-
+n = int(input())
+arr = [0]
+for i in range(n):
+    arr.append(int(input()))    
+dp=[0]
+dp.append(arr[1])
+if n > 1:
+    dp.append(arr[1]+arr[2])
+for i in range (3,n+1):
+    dp.append(max(dp[i-1],dp[i-3]+arr[i-1]+arr[i],dp[i-2]+arr[i]))
+print(dp[n])
